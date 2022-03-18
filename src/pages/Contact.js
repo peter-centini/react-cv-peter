3613db3a28e0a461fe2fc3f5d1831adaef469aad
Peter <peter@MacBook-Air-de-Peter.local>
2022-03-18 18:41:36 +0100

    finished

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -46,7 +46,7 @@ const Contact = () => {
         <div className="socialNetwork">
           <ul>
             <a
-              href="https://github.com/peter-centini"
+              href="https://www.linkedin.com/in/peter-centini-ab0695215"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -54,7 +54,7 @@ const Contact = () => {
               <i className="fab fa-linkedin"></i>
             </a>
             <a
-              href="https://www.linkedin.com/in/peter-centini-ab0695215"
+              href="https://github.com/peter-centini"
               target="_blank"
               rel="noopener noreferrer"
             >
